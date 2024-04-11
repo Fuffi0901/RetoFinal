25420fd78cfd1181c243b527ee1e8ca15b23e784
GitEjercicios <GitEjercicios@gmail.com>
2024-04-11 08:33:13 +0200

    Versión 1 de Guille.

---
+++ RetoFinal/src/controlador/Main.java
@@ -0,0 +1,14 @@
+package controlador;
+
+import vista.Inicio_Sesion;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/**
+		 * Launch the application.
+		 */
+		Inicio_Sesion ven = new Inicio_Sesion();
+		ven.setVisible(true);
+	}
+}
